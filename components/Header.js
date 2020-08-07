874625fb8872daf831a9e6fa5efcd606ba7967bb
Lambda_School_Loaner_271 <lambda_school_loaner_271@Lambda-School-Loaner-271s-MacBook-Pro.local>
2020-08-07 11:26:06 -0400

    finished header

--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,31 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+ 
+const appendHereHeader = document.querySelector('.header-container')
 
-function Header() {}
+function Header() {
+    
+    const mainDiv = document.createElement('div')
+    const date = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    mainDiv.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    date.textContent = 'March 28, 2020'
+    h1.textContent = 'Lambda Times'
+    temp.textContent= '98˚'
+
+    mainDiv.appendChild(date)
+    mainDiv.appendChild(h1)
+    mainDiv.appendChild(temp)
+    
+    return mainDiv
+
+}
+appendHereHeader.appendChild(Header());
+    
+
